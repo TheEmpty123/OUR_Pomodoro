cd5c9f555696660451a3f8d6f2dd45c4518eb7fb
Hai-Hay-Ho <hominhhai2k4@gmail.com>
2025-04-18 20:09:55 +0700

    upload code post to do #FR3

--- Backend/pomodoro/src/main/java/com/mobile/pomodoro/controller/ToDoController.java
+++ Backend/pomodoro/src/main/java/com/mobile/pomodoro/controller/ToDoController.java
@@ -1,9 +1,13 @@
 package com.mobile.pomodoro.controller;
 
+import com.mobile.pomodoro.dto.request.ToDoRequestDTO;
+import com.mobile.pomodoro.dto.response.MessageResponseDTO;
 import com.mobile.pomodoro.dto.response.ToDoResponeseDTO.ToDoResponseDTO;
 import com.mobile.pomodoro.entities.User;
 import com.mobile.pomodoro.services.IToDoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,9 +23,8 @@ public class ToDoController {
     }
 
 
-//    @PostMapping("create")
-//    public MessageResponseDTO createTodo(@RequestAttribute(name = "user") User user,
-//                                         @RequestBody ToDoRequestDTO todoRequestDTO) {
-//        return toDoService.createTodo(user, todoRequestDTO);
-//    }
+    @PostMapping("create")
+    public ResponseEntity<MessageResponseDTO> register(@RequestBody ToDoRequestDTO requestDTO,@RequestHeader("username") String username) {
+        return new ResponseEntity<>(toDoService.createToDo(requestDTO,username), HttpStatus.CREATED);
+    }
 }
--- Backend/pomodoro/src/main/java/com/mobile/pomodoro/services/impl/ToDoServiceImpl.java
+++ Backend/pomodoro/src/main/java/com/mobile/pomodoro/services/impl/ToDoServiceImpl.java
@@ -1,19 +1,33 @@
 package com.mobile.pomodoro.services.impl;
+
 import com.mobile.pomodoro.CustomException.UserNotFoundException;
+import com.mobile.pomodoro.dto.request.ToDoRequestDTO;
+import com.mobile.pomodoro.dto.response.MessageResponseDTO;
 import com.mobile.pomodoro.dto.response.ToDoResponeseDTO.ToDoResponseDTO;
 import com.mobile.pomodoro.dto.response.ToDoResponeseDTO.ToDoResponseDTO.SingleToDoDTO;
 import com.mobile.pomodoro.entities.Todo;
+import com.mobile.pomodoro.entities.User;
 import com.mobile.pomodoro.repositories.ToDoRepository;
+import com.mobile.pomodoro.repositories.UserRepository;
 import com.mobile.pomodoro.services.IToDoService;
+import com.mobile.pomodoro.services.IUserService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
 @Service
 public class ToDoServiceImpl extends AService implements IToDoService {
     @Autowired
     private ToDoRepository toDoRepository;
+    @Autowired
+    private IUserService IUser;
+    @NonNull
+    HttpServletRequest request;
 
 
     @Override
@@ -36,5 +50,28 @@ public class ToDoServiceImpl extends AService implements IToDoService {
         return ToDoResponseDTO.builder()
                 .list(todoDTOs)
                 .build();
+    }
+
+    @Override
+    public MessageResponseDTO createToDo(ToDoRequestDTO requestDTO,String username) {
+        try {
+            User user = IUser.getUserByUsername(username);
+            Long userId = user.getUserId();
+            Todo todo = Todo.builder()
+                    .userId(userId)
+                    .title(requestDTO.getTitle())
+                    .isDone(0)
+                    .build();
+            toDoRepository.save(todo);
+
+            return MessageResponseDTO.builder()
+                    .message("Thêm thành công hjhj")
+                    .build();
+
+        } catch (Exception e) {
+            return MessageResponseDTO.builder()
+                    .message("Lỗi khi thêm ToDo: " + e.getMessage())
+                    .build();
+        }
     }
 }
