560bc4731dc3b919c5487588075223ddcd57925f
Hai-Hay-Ho <hominhhai2k4@gmail.com>
2025-05-08 21:58:46 +0700

    upload code fr2

--- Backend/pomodoro/src/main/java/com/mobile/pomodoro/services/IPlanService.java
+++ Backend/pomodoro/src/main/java/com/mobile/pomodoro/services/IPlanService.java
@@ -3,9 +3,8 @@ package com.mobile.pomodoro.services;
 import com.mobile.pomodoro.CustomException.UserNotFoundException;
 import com.mobile.pomodoro.dto.request.PlanRequestDTO;
 import com.mobile.pomodoro.dto.response.MessageResponseDTO;
-import com.mobile.pomodoro.dto.response.PlanResponeseDTO.PlanResponeseDTO;
+import com.mobile.pomodoro.dto.response.PlanTaskResponeseDTO.PlanTaskResponeseDTO;
 import com.mobile.pomodoro.dto.response.PlanResponseDTO.PlanResponseDTO;
-import com.mobile.pomodoro.dto.response.TaskToEditResponseDTO.TaskToEditResponseDTO;
 import com.mobile.pomodoro.entities.User;
 
 public interface IPlanService extends IInitializerData{
@@ -14,4 +13,5 @@ public interface IPlanService extends IInitializerData{
     public PlanResponseDTO findPlan(String username) throws UserNotFoundException;
     MessageResponseDTO createPlan(PlanRequestDTO requestDTO, User user);
     PlanResponseDTO findRecentPlan(String username) throws UserNotFoundException;
+    PlanTaskResponeseDTO processWithoutSaving(PlanRequestDTO requestDTO, User user);
 }
