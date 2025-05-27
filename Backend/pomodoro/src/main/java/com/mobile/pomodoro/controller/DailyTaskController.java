aea791378ce156bbbae65468f68f15f04534816d
Bao <thaibaothptdl@gmail.com>
2025-05-27 11:18:46 +0700

    add endpoint

--- Backend/pomodoro/src/main/java/com/mobile/pomodoro/controller/DailyTaskController.java
+++ Backend/pomodoro/src/main/java/com/mobile/pomodoro/controller/DailyTaskController.java
@@ -1,11 +1,13 @@
 package com.mobile.pomodoro.controller;
-
 import com.mobile.pomodoro.dto.request.DailyTaskRequestDTO;
 import com.mobile.pomodoro.dto.response.DailyTaskResponeseDTO.DailyTaskResponeseDTO;
 import com.mobile.pomodoro.dto.response.MessageResponseDTO;
+import com.mobile.pomodoro.dto.response.PlanToEditResponseDTO.PlanToEditResponseDTO;
 import com.mobile.pomodoro.entities.User;
 import com.mobile.pomodoro.services.IDailyTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,4 +28,15 @@ public class DailyTaskController {
             @RequestAttribute(name = "user") User user) {
         return dailyTaskService.createDailyTask(request, user.getUserId());
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<PlanToEditResponseDTO> getDailyTaskPlanDetails(@PathVariable Long id,
+                                                                         @RequestAttribute(name = "user") User user) {
+        try {
+            return new ResponseEntity<>(dailyTaskService.getDailyTaskPlanDetails(id, user), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
 }
