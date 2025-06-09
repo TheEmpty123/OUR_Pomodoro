41ed6ef32555d0e19866f2b43f13dd15b0a2d40e
toang <toangaming.203@gmail.com>
2025-06-10 03:51:07 +0700

    update: assign enum to global access, fix entity assign in appdatabase

--- FE/app/src/main/java/com/mobile/pomodoro/room/AppDatabase.java
+++ FE/app/src/main/java/com/mobile/pomodoro/room/AppDatabase.java
@@ -3,11 +3,13 @@ package com.mobile.pomodoro.room;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import com.mobile.pomodoro.room.entity.TodoItem;
+import com.mobile.pomodoro.room.entity.User;
 import com.mobile.pomodoro.room.repo.TodoRepository;
 import com.mobile.pomodoro.room.repo.UserRepository;
 
 
-@Database(entities = {UserRepository.class, TodoRepository.class}, version = 5, exportSchema = false)
+@Database(entities = {User.class, TodoItem.class}, version = 5, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract UserRepository user();
     public abstract TodoRepository todoItem();
