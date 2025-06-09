dc0ed4bb27b669335f413ecec1f23f5810924252
toang <toangaming.203@gmail.com>
2025-06-10 03:16:10 +0700

    fix: appdata func from entity to repo

--- FE/app/src/main/java/com/mobile/pomodoro/room/AppDatabase.java
+++ FE/app/src/main/java/com/mobile/pomodoro/room/AppDatabase.java
@@ -3,12 +3,12 @@ package com.mobile.pomodoro.room;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-import com.mobile.pomodoro.room.entity.TodoItem;
-import com.mobile.pomodoro.room.entity.User;
+import com.mobile.pomodoro.room.repo.TodoRepository;
+import com.mobile.pomodoro.room.repo.UserRepository;
 
 
-@Database(entities = {User.class, TodoItem.class}, version = 5, exportSchema = false)
+@Database(entities = {UserRepository.class, TodoRepository.class}, version = 5, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
-    public abstract User user();
-    public abstract TodoItem todoItem();
+    public abstract UserRepository user();
+    public abstract TodoRepository todoItem();
 }
